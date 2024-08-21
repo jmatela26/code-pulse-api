ac23565b7e4f918a7ffa74dd8459e7d45bc210de
jmatela26 <jmatela26@gmail.com>
2024-08-21 21:41:04 +0800

    Added create category logic

--- CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
@@ -7,15 +10,35 @@ namespace CodePulse.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        public CategoriesController()
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
         {
-            
+            _dbContext = dbContext;
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateCategory()
+        public async Task<IActionResult> CreateCategory([FromBody]CreateCategoryRequestDto request)
         {
-            return Ok();
+            // Map DTO to Domain Model
+            var category = new Category()
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            await _dbContext.Categories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+
+            // Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            return Ok(response);
         }
     }
 }
