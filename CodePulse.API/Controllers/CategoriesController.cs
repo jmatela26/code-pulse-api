d18eba5f70399375652dcbc3537a55a49226e9cc
jmatela26 <jmatela26@gmail.com>
2024-08-21 21:25:40 +0800

    Add project files.

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        public CategoriesController()
+        {
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory()
+        {
+
+        }
+    }
+}
